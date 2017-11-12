daa7c7827654a15fa7e5bf66ca71c776a3549623
DengWangBao <519526708@qq.com>
2017-11-12 16:15:55 +0800

    Initial commit

---
+++ app/src/main/java/com/sz/alpha/cirquepiechart/MainActivity.java
@@ -0,0 +1,36 @@
+package com.sz.alpha.cirquepiechart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    protected void init() {
+        initView();
+        initData();
+        initEvent();
+    }
+
+    //初始化视图
+    protected void initView() {
+
+    }
+
+    //初始化数据
+    private void initData() {
+
+    }
+
+    //初始化事件
+    protected void initEvent() {
+
+    }
+
+}
