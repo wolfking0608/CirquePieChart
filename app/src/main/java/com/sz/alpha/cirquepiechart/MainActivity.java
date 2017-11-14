f7da81d82bd8e2440f674e8a240a82646a71233c
wolfking0608 <wolfking0608@qq.ccom>
2017-11-14 18:48:29 +0800

    king
    init

--- app/src/main/java/com/sz/alpha/cirquepiechart/MainActivity.java
+++ app/src/main/java/com/sz/alpha/cirquepiechart/MainActivity.java
@@ -1,14 +1,22 @@
 package com.sz.alpha.cirquepiechart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvv)
+    TextView mTvv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         init();
     }
 
